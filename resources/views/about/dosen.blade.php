e5dda2572ed506a2aa3ec912c97ea20b73d50ce7
elan <elandramaulana81@gmail.com>
2023-12-19 16:59:31 +0700

    update apis

--- resources/views/about/dosen.blade.php
+++ resources/views/about/dosen.blade.php
@@ -1,7 +1,7 @@
 @extends('layout.layoutmain')
 
 @section('content')
-<div class="breadcrumbs">
+<div class="breadcrumbs mb-5">
     <nav>
       <div class="container">
         <ol>
@@ -11,5 +11,54 @@
       </div>
     </nav>
 </div>  
+
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-6 col-lg-4 mb-4">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="..." class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Elan Gamtenk</h5>
+                            <p class="card-text">Depertement: </p>
+                            <p class="card-text">Email: </p>
+                            <p class="card-text">Publikasi: </p>
+                            <p class="card-text">Edukasi: </p>
+                            <p class="card-text">Penelitian: </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-6 col-lg-4 mb-4">
+            <div class="card mb-3" style="max-width: 540px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                        <img src="..." class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Elan Gamtenk</h5>
+                            <p class="card-text">Depertement: </p>
+                            <p class="card-text">Email: </p>
+                            <p class="card-text">Publikasi: </p>
+                            <p class="card-text">Edukasi: </p>
+                            <p class="card-text">Penelitian: </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
 @endsection
 
