28a7885a936162845b19a0e6dbdfc9ffe890cc81
elan <elandramaulana81@gmail.com>
2023-12-19 17:04:30 +0700

    apis

--- resources/views/prodi/s1-pyk/tujuan-prodi-s1pyk.blade.php
+++ resources/views/prodi/s1-pyk/tujuan-prodi-s1pyk.blade.php
@@ -6,10 +6,45 @@
       <div class="container">
         <ol>
           <li><a href="/">Beranda</a></li>
-          <li>Tujuan Prodi S1 Payakumbuh</li>
+          <li>Tujuan Prodi</li>
         </ol>
       </div>
     </nav>
 </div>  
-@endsection
+<article class="single-post">
+    <div class="container">
+        <div class="post-heading">
+            <h2>Tujuan</h2>
+            <ul class="entry-meta">
+                <li class="post-date"> <!-- data tanggal upload-->
+                    <i class='bx bx-time-five'></i>
+                    8 November 2023, 13.00
+                </li>
+                <li class="post-autor"> <!-- data admin yang upload dan terhubung ke admin-->
+                    <i class='bx bxs-user'></i>
+                    Oleh:
+                    <a href="#">admin</a>
+                </li>
+            </ul>
+        </div>
+        <div class="post-content">
+            <blockquote>
+                <h4>
+                    <span style="color:#87566e">
+                        <strong>
+                        Tujuan dari Prodi Peternakan Kampus II Payakumbuh
+                        </strong>
+                    </span>
+                </h4>
+            </blockquote>
+        </div>
+        <ol>
+          <li>Menghasilkan sarjana peternakan yang unggul dan bermoral, memiliki kompetensi global yang berorientasi sumberdaya lokal.</li>
+          <li>Menghasilkan karya penelitian dan inovasi teknologi berorientasi sumberdaya lokal secara optimal dan berkelanjutan.</li>
+          <li>Meningkatnya kualitas dan kuantitas pengabdian kepada masyarakat untuk meningkatkan daya saing peternakan rakyat</li>
+          <li>Meningkatkan minat masyarakat untuk mendalami ilmu dan teknologi dan membangun profesi bidang usaha yang terkait dengan peternakan untuk menciptakan peluang kerja dan ketahanan pangan.</li>
+        </ol>
 
+    </div>
+</article>
+@endsection
--- resources/views/prodi/s3/sejarah-prodi-s3.blade.php
+++ resources/views/prodi/s3/sejarah-prodi-s3.blade.php
@@ -6,10 +6,43 @@
       <div class="container">
         <ol>
           <li><a href="/">Beranda</a></li>
-          <li>Sejarah Prodi S3</li>
+          <li>Sejarah</li>
         </ol>
       </div>
     </nav>
 </div>  
+<article class="single-post">
+    <div class="container">
+        <div class="post-heading">
+            <h2>Sejarah Program Studi S3 Ilmu Peternakan</h2>
+            <ul class="entry-meta">
+                <li class="post-date"> <!-- data tanggal upload-->
+                    <i class='bx bx-time-five'></i>
+                  8 November 2023, 13.00
+                </li>
+                <li class="post-autor"> <!-- data admin yang upload dan terhubung ke admin-->
+                    <i class='bx bxs-user'></i>
+                    Oleh:
+                    <a href="#">admin</a>
+                </li>
+            </ul>
+        </div>
+        <div class="post-content">
+            <blockquote>
+                <h4>
+                    <span style="color:#87566e">
+                        <strong>
+                        Sejarah Program Studi S3 Ilmu Peternakan
+                        </strong>
+                    </span>
+                </h4>
+            </blockquote>
+        </div>
+        <p style="text-align: justify">
+        Sebagai salah satu negara yang memiliki sumber daya alam  sangat besar, Indonesia menyediakan banyak jenis ternak dan pakan ternak yang tidak ternilai harganya, seperti sapi dan unggas lokal. Semua ternak lokal ini merupakan sumber daya  genetik yang perlu dipertahankan dan dikembangkan sebagai plasma nutfah. Disamping itu potensi dari ternak crossbred yang berasal dari hasil persilangan dengan ternak unggul (impor) yang menghadapi kendala – kendala terutama dari aspek reproduksi dan fertilitas. Hal ini sangat dibutuhkan untuk diteliti dan ditemukan serta diterapkan teknologi akan meningkatan produktivitas ternak lokal.Potensi  yang tersimpan ini dapat diangkat untuk tujuan pelestarian lingkiungan maupun untuk kesejahteraan manusia.        </p>
+        <p style="text-align: justify">
+        Untuk mengantisipasi kemajuan ilmu dan teknologi yang sangat pesat, perlu disiapkan kemampuan ber­saing yang tinggi, maka pendidikan Peternakan harus menekankan penguasaan ilmu multidisiplin seperti biokimia, animal breeding, bioteknologi reproduksi dan mikrobiologi serta teknologi nutrisi dan pakan ternak maupun  perencanaan dan pengembangan peternakan . Kebijakan ini harus di­tempuh, karena ilmu – ilmu tersebut dengan penerapan teknologi mutakhir  akan menjadikan harapan ilmu masa depan. Untuk itu program studi S3 ilmu peternakan sangat dibutuhkan dalam rangka memenuhi kebutuhan tenaga peneliti peternakan.        </p>
+       
+    </div>
+</article>
 @endsection
-
