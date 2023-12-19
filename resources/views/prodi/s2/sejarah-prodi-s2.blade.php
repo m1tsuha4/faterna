e131b7883f8ef860fcf79e210a307b033ca0f9ab
apiz <alhafizzhurrahman@gmail.com>
2023-12-19 15:46:32 +0700

    apis

--- resources/views/prodi/s2/sejarah-prodi-s2.blade.php
+++ resources/views/prodi/s2/sejarah-prodi-s2.blade.php
@@ -6,10 +6,51 @@
       <div class="container">
         <ol>
           <li><a href="/">Beranda</a></li>
-          <li>Sejarah Prodi S2</li>
+          <li>Sejarah</li>
         </ol>
       </div>
     </nav>
 </div>  
+<article class="single-post">
+    <div class="container">
+        <div class="post-heading">
+            <h2>Sejarah Program Studi S2 Ilmu Peternakan</h2>
+            <ul class="entry-meta">
+                <li class="post-date"> <!-- data tanggal upload-->
+                    <i class='bx bx-time-five'></i>
+                  8 November 2023, 13.00
+                </li>
+                <li class="post-autor"> <!-- data admin yang upload dan terhubung ke admin-->
+                    <i class='bx bxs-user'></i>
+                    Oleh:
+                    <a href="#">admin</a>
+                </li>
+            </ul>
+        </div>
+        <div class="post-content">
+            <blockquote>
+                <h4>
+                    <span style="color:#87566e">
+                        <strong>
+                        Sejarah Program Studi S2 Ilmu Peternakan
+                        </strong>
+                    </span>
+                </h4>
+            </blockquote>
+        </div>
+        <p style="text-align: justify">
+        Program Studi S2 Ilmu Peternakan, Program Pascasarjana, Universitas Andalas berdiri sejak 27 Oktober 1992, melalui Surat Keputusan Direktorat Jenderal Pendidikan Tinggi No. 481/Dikti/Kep/1992. Program Studi ini merupakan Program Studi kedua pada Program Pascasarjana Universitas Andalas.  Sebelumnya Program Studi S2 Ilmu Peternakan bernama Program Studi Ilmu Ternak, dan sejak tahun 2007 melalui SK Dirjen Dikti No. 163/Dikti/Kep/2007 tentang penataan ulang Program Studi di Fakultas Peternakan yaitu sejak dileburnya 4 program studi S1 menjadi 1 program studi yaitu Program Studi S1 Peternakan, maka program studi magister disebut dengan Program Studi S2 Ilmu Peternakan.        </p>
+        <p style="text-align: justify">
+        Sampai saat ini perkembangan Program Studi S2 Ilmu Peternakan ini masih belum mengalami perkembangan sesuai dengan yang diharapkan. Permasalahan pada saat ini adalah masih rendahnya peminat masuk program studi S2 Ilmu Peternakan setiap tahun. Sementara itu kapasitas dan kualitas dosen, tenaga kependidikan dan fasilitas pendukung proses belajar mengajar tersedia secara memadai. Dalam menyikapi permasalahan tersebut, maka diperlukan langkah positif, dan konstruktif yang sistematis untuk dapat lebih berkembang sesuai dengan yang diharapkan.        </p>
+        <p style="text-align: justify">
+        Salah satu upaya meningkatkan jumlah peminat adalah dengan selalu melakukan promosi mengenai kapasitas yang dimiliki oleh program studi S2 Ilmu Peternakan ini dan perannya dalam pengembangan sumberdaya manusia dan pembangunan peternakan yang berkualitas melalui iklan di koran, website, spanduk dan roadshow. Disamping itu juga dilakukan Kerjasama dengan pemerintah daerah dalam membantu meningkatkan kemampuan staf yang dimilikinya di bidang ilmu Peternakan melalui penawaran pendidikan pada program studi S2 Ilmu Peternakan.        </p>
+        <p style="text-align: justify">
+        Program studi S2 Ilmu Peternakan ke depan ingin berperan lebih aktif lagi dalam meningkatkan kemampuan sumberdaya manusia bidang Peternakan yang handal dan mampu bersaing di tingkat nasional.  Disamping itu program studi juga menginginkan keterlibatan yang intens dari sumberdaya manusia yang dihasil program studi ini untuk berperan lebih aktif dalam menggali potensi sumberdaya Peternakan lokal yang dapat dikembangkan dan dimanfaatkan dalam pembangunan Peternakan di Indonesia.        </p>
+        <p style="text-align: justify">
+        Dalam rangka mewujudkan keinginan tersebut maka disusunlah Rencana Strategis Program Studi S2 Ilmu Peternakan tahun 2009 sampai 2014. Rencana strategis menyajikan permasalahan, faktor pendukung, tujuan strategis, strategi pengembangan, arah kebijakan umum dan program pembangunan peternakan. Dengan demikian diharapkan Rencana Strategis ini dapat menjadi acuan dalam penyelenggaraan Program Studi S2 Ilmu Peternakan, Program Pascasarjana, Universitas Andalas lima tahun ke depan.        </p>
+        
+       
+        
+    </div>
+</article>
 @endsection
-
--- resources/views/prodi/s2/visi-misi-prodi-s2.blade.php
+++ resources/views/prodi/s2/visi-misi-prodi-s2.blade.php
@@ -6,10 +6,62 @@
       <div class="container">
         <ol>
           <li><a href="/">Beranda</a></li>
-          <li>Visi Misi Prodi S2</li>
+          <li>Visi Misi </li>
         </ol>
       </div>
     </nav>
 </div>  
-@endsection
+<article class="single-post">
+    <div class="container">
+        <div class="post-heading">
+            <h2>Visi Misi Prodi S2 Peternakan</h2>
+            <ul class="entry-meta">
+                <li class="post-date"> <!-- data tanggal upload-->
+                    <i class='bx bx-time-five'></i>
+                    8 November 2023, 13.00
+                </li>
+                <li class="post-autor"> <!-- data admin yang upload dan terhubung ke admin-->
+                    <i class='bx bxs-user'></i>
+                    Oleh:
+                    <a href="#">admin</a>
+                </li>
+            </ul>
+        </div>
+        <div class="post-content">
+            <blockquote>
+                <h4>
+                    <span style="color:#87566e">
+                        <strong>
+                            Visi
+                        </strong>
+                    </span>
+                </h4>
+            </blockquote>
+        </div>
+        <p style="text-align: justify">
+        “Menjadi Institusi Penyelenggara Program Magister Ilmu Peternakan yang mampu menghasilkan Magister Peternakan yang memiliki daya saing ditingkat Nasional dan Internasional serta berperan aktif dalam pengembangan dan pembangunan peternakan tropis pada tahun 2025”.
+        </p>
+        <div class="post-content">
+            <blockquote>
+                <h4>
+                    <span style="color:#87566e">
+                        <strong>
+                            Misi
+                        </strong>
+                    </span>
+                </h4>
+            </blockquote>
+        </div>
+        <p style="text-align: justify">
+        Misi Universitas Andalas dan Fakultas Peternakan Universitas Andalas adalah melaksanakan Tri Dharma Perguruan Tinggi, yaitu Pendidikan, Penelitian dan Pengabdian kepada masyarakat dalam rangka meningkatkan IPTEKS dan menciptakan sumberdaya manusia yang berkualitas untuk terwujudnya kejayaan bangsa. Sesuai dengan misi Universitas dan Fakultas Peternakan Universits Andalas, maka misi Program Studi S2 Ilmu Peternakan adalah
+        </p>
+        <ol>
+        <li>Menyelenggarakan pendidikan ilmu peternakan jenjang Magister (S2) sesuai dengan kebutuhan pengembangan sumberdaya manusia untuk pembangunan peternakan di daerah tropis.</li>
+        <li>Melaksanakan penelitian dasar, terapan dan pengembangan berbasis sumberdaya lokal untuk pengembangan peternakan yang berkelanjutan serta mempublikasikannya di tingkat nasional dan internasional.</li>
+        <li>Menyelenggarakan kegiatan pengabdian kepada masyarakat berbasis IPTEKS yang diperoleh dari hasil penelitian yang dapat diadopsi oleh masyarakat.</li>
+        <li>Membangun jejaring kerjasama (network) dengan pihak pemerintah, swasta serta lembaga Perguruan Tinggi baik di dalam maupun luar negeri, untuk meningkatkan kualitas penyelenggaraan proses pendidikan, penelitian dan pengabdian kepada masyarakat.</li>
+        </ol>
 
+    </div>
+</article>
+@endsection
